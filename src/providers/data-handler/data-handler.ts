bff6885764d1de75350f80edfdc85c8c7e5d82ca
akrapi <akrapi@uth.gr>
2018-07-01 00:50:58 +0300

    medsaqfee

--- src/providers/data-handler/data-handler.ts
+++ src/providers/data-handler/data-handler.ts
@@ -1,5 +1,5 @@
 //import { HttpClient } from '@angular/common/http';
-import { Http,Response } from '@angular/http';
+import { Http} from '@angular/http';
 import { Injectable } from '@angular/core';
 //import { Observable } from 'rxjs/Observable';
 import 'rxjs/Rx';
